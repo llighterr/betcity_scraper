d85069a6fad460f20c08917a4ac5a2d324260172
Ihor Strumetskyi <igors@roundforest.com>
2020-04-10 14:44:23 +0300

    fix: enable headless

--- capybara_config.rb
+++ capybara_config.rb
@@ -11,7 +11,7 @@ $proxy = server.create_proxy
 Capybara.register_driver :selenium_proxy do |app|
   Capybara::Selenium::Driver.load_selenium
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-    # opts.args << '--headless'
+    opts.args << '--headless'
     opts.args << '--disable-gpu' if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.args << '--disable-site-isolation-trials'
