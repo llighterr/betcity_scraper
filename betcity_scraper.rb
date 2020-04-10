ce721a63ebeb2c3d33f7bf4cc0730f10544c9058
Ihor Strumetskyi <igors@roundforest.com>
2020-04-10 14:37:30 +0300

    feat: implement scraper

--- betcity_scraper.rb
+++ betcity_scraper.rb
@@ -1,35 +1,115 @@
-require 'capybara'
+require 'bigdecimal'
 require 'capybara/dsl'
-require 'pry'
-
-# Capybara.register_driver :selenium_chrome_headless
-Capybara.run_server = false
-Capybara.default_driver = :selenium_chrome_headless
 
 class BetcityScraper
   include Capybara::DSL
 
   SITE_URL = 'https://betcityru.com/ru?login=1'
+  DEFAULT_BET_RANGE = (10..200)
+  DEFAULT_TIMEOUT = 10
+  NO_BETS_ALLOWED = 'Приём ставок временно остановлен'
 
-  def initialize(account_number, password)
-    @agent = initialize_agent
+  def initialize(account_number, password, proxy:)
     @account_number = account_number
-    @password = password    
+    @password = password
+    @proxy = proxy
   end
 
   def login
-    binding.pry
+    visit SITE_URL
+    close_popup
+    fill_in 'login', with: @account_number
+    fill_in 'pass', with: @password
+    click_button 'Войти'
+  end
+
+  def balance
+    path = '//span[contains(text(),"Баланс")]/following-sibling::span[1]'
+    amount = find(:xpath, path).text.delete(' ').sub(',', '.').delete('руб')
+    BigDecimal(amount)
+  end
+
+  def list_of_live_events(unacceptable_bets: false)
+    close_popup
+    container_path = '//app-live-block/div/div[contains(@class,"line__wrapper")]'
+    events = find(:xpath, container_path).all('a.line-event__name-link')
+    unacceptable_bets ? filter_unacceptable_bets(events) : events
+  end
+
+  def set_random_bet_for(link)
+    retries ||= 0
+    sleep 1
+
+    visit link
+    sleep 1
+    event_id ||= current_path.split('/').last
+    amount ||= [rand(DEFAULT_BET_RANGE), balance.to_i].min
+    status ||= :pending
+
+    button_path = "//app-dop-ext[.//span[contains(text(),'Фактический исход')]]"\
+                  "//div[@data-first-index='Wm' and ./span[contains(text(),"\
+                  "'1')]]/button"
+    find(:xpath, button_path).click
+    fill_in 'Сумма', with: amount
+    click_button 'сделать ставку'
+
+    Timeout.timeout(DEFAULT_TIMEOUT) do
+      sleep 0.5 until has_content? 'Принято: 1'
+    end
+    status = :success
+    { event_id: event_id, status: status, amount: amount }
+  rescue Capybara::ElementNotFound, Net::HTTPBadResponse
+    if has_content? NO_BETS_ALLOWED
+      status = :failure
+    elsif retries < 2
+      retries += 1
+      retry
+    else
+      status = :failure
+    end
+    { event_id: event_id, status: status, amount: amount }
+  rescue Timeout::Error
+    status = :failure
+    { event_id: event_id, status: status, amount: amount }
   end
 
   private
 
-  attr_reader :agent
+  def close_popup
+    first(:xpath, '//div[@role="button" and contains(text(),"Позже")]').click
+  rescue Capybara::ExpectationNotMet
+  end
 
-  def initialize_agent
-    
+  def filter_unacceptable_bets(events)
+    events.delete_if do |event|
+      event.find(:xpath, './../../../..').text =~ Regexp.new(NO_BETS_ALLOWED)
+    end
   end
 end
 
 if $0 == __FILE__
-  BetcityScraper.new('g8657706', '11707844').login
+  require 'pp'
+  require 'redis'
+  require 'json'
+
+  load './capybara_config.rb'
+
+  bet_city = BetcityScraper.new('g8657706', '11707844', proxy: $proxy)
+  bet_city.login
+  puts "Текущий баланс: #{'%.2f' % bet_city.balance}"
+
+  events_amount = 4
+  links = bet_city.list_of_live_events.sample(events_amount).map do |event|
+    event[:href]
+  end
+
+  data = links.collect do |link|
+    bet_city.set_random_bet_for(link)
+  end
+
+  puts 'Статус оппераций:'
+  pp data
+
+  redis = Redis.new
+  redis.set('results', data.to_json)
 end
---
+++ capybara_config.rb
@@ -0,0 +1,27 @@
+
+require 'capybara'
+require 'browsermob/proxy'
+require 'browsermob/proxy/webdriver_listener'
+
+path = Dir.pwd + "/browserup-proxy-2.0.1/bin/browserup-proxy"
+server = BrowserMob::Proxy::Server.new(path)
+server.start
+$proxy = server.create_proxy
+
+Capybara.register_driver :selenium_proxy do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    # opts.args << '--headless'
+    opts.args << '--disable-gpu' if Gem.win_platform?
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.args << '--disable-site-isolation-trials'
+    opts.args << '--ignore-certificate-errors'
+    opts.args << '--proxy-server=http://' + $proxy.selenium_proxy.http
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :selenium_proxy
+end
